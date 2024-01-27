e2b55d14864b7f7285401d8d7301fc1723d34789
RadoslavDimitrovYankov <133864010+RadoslavDimitrovYankov@users.noreply.github.com>
2024-01-27 22:18:36 +0200

    Add project files.

---
+++ QuessTheNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿
+Random randomNumber = new Random();
+int computerNumber = randomNumber.Next(1, 101);
+
+while (true)
+{
+    Console.Write("Guess a number (1 - 100): ");
+    string playrInput = Console.ReadLine();
+    bool isValid = int.TryParse(playrInput, out int playerNumber);
+
+    if (isValid)
+    {
+        if (computerNumber == playerNumber)
+        {
+            Console.WriteLine("You guessed it!");
+            break;
+        }
+        else if (playerNumber > computerNumber)
+        {
+            Console.WriteLine("Too High");
+        }
+        else
+        {
+            Console.WriteLine("Too Low");
+        }
+    }
+
+
+}
